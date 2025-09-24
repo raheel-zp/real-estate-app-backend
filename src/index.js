f9d9429b4d8bcf61085ae33a91216114090cdaf4
Mirza Raheel <raheel1195@gmail.com>
2025-09-24 18:55:04 +0500

    changes for vercel added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 const { connectMongo } = require("./config/db");
 const propertiesRouter = require("./routes/properties");
@@ -9,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 app.use("/api/properties", propertiesRouter);
 const authRoutes = require("./routes/authRoutes");
