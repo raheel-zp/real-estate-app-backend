a7b51fd92c24c1ea2cf96b574a652d90bca1a3f5
Mirza Raheel <raheel1195@gmail.com>
2025-09-24 18:43:36 +0500

    changes for vercel added

--- src/index.js
+++ src/index.js
@@ -1,54 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require("path");
 dotenv.config();
 const { connectMongo } = require("./config/db");
 const propertiesRouter = require("./routes/properties");
-const authRoutes = require("./routes/authRoutes");
-const favoritesRouter = require("./routes/favorites");
-const inquiryRoutes = require("./routes/inquiryRoutes");
 
 const app = express();
-const allowedOrigins = [
-  "https://real-estate-app-frontend-chi.vercel.app",
-  "http://localhost:5173",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.options("*", cors());
-
+app.use(cors());
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/properties", propertiesRouter);
+const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
+
+const favoritesRouter = require("./routes/favorites");
 app.use("/api/favorites", favoritesRouter);
+
+const inquiryRoutes = require("./routes/inquiryRoutes");
 app.use("/api/inquiries", inquiryRoutes);
 
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV === "development") {
-  connectMongo().then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server listening on ${PORT}`);
-    });
+connectMongo().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
   });
-} else {
-  connectMongo();
-  module.exports = app;
-}
+});
